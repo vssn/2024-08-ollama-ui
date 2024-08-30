d9f7b8a36837445681c8110d6fa211a1b537a1bb
Edmund <matrim.eg@gmail.com>
2024-08-30 20:10:28 +0000

    Working MVP

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Chat from './Chat'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Little language model</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Chat></Chat>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Enter a prompt to get an AI model response.
       </p>
     </>
   )
---
+++ src/Chat.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import * as env from "./../env.json";
+
+const Chat = () => {
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
+    const handleSendMessage = async (e) => {
+        e.preventDefault();
+        // setMessages((prevMessages) => [...prevMessages, newMessage]);
+        getData(newMessage);
+        setNewMessage('');
+    };
+
+    async function getData(prompt) {
+        const decoder = new TextDecoder('utf-8');
+      
+        try {
+          const response = await fetch(`${env.url}/api/generate`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              model: 'llama3.1',
+              prompt
+            })
+          });
+            
+          const reader = response.body.getReader();
+            
+          while (true) {
+              const { done, value } = await reader.read();
+              if (done) {
+                setMessages((prevMessages) => [...prevMessages, <><br /><br /></>]);
+
+                return;
+              }
+
+              const sanitizedValue = decoder.decode(value)
+              const newMessage = JSON.parse(sanitizedValue).response;
+              setMessages((prevMessages) => [...prevMessages, newMessage]);
+            }
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+
+    return (
+        <div>
+            <div className="answer-output">
+                {messages.map((message, index) => (
+                    <span key={index}>{message}</span>
+                ))}
+            </div>
+            <form onSubmit={handleSendMessage}>
+                <input
+                    type="text"
+                    className="prompt-input"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    placeholder="Prompt the llama"
+                    />
+                <button type="button" onClick={handleSendMessage}>Send</button>
+            </form>
+        </div>
+    );
+};
+
+export default Chat;
